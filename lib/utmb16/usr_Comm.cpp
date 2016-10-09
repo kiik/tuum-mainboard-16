9dbd2506794218e1545a57c7a504c297c9428758
Meelik Kiik <kiik.meelik@gmail.com>
2016-10-09 16:59:47 +0300

    Update 'usr_comm' - implement callback router.

--- lib/utmb16/usr_Comm.cpp
+++ lib/utmb16/usr_Comm.cpp
@@ -13,13 +13,72 @@
 
 namespace usr {
 
-  void msg_ack(const rtx::Protocol::Message& msg) {
+  Comm::cmd_map_t Comm::loadCommandMap() {
+    Comm::cmd_map_t out;
+
+    out["sp"] = ECMD_SetSpeed;
+    out["mv"] = ECMD_Move;
+    out["tr"] = ECMD_Turn;
+    out["om"] = ECMD_Omni;
+
+    out["pd"] = ECMD_SetPid;
+    out["en"] = ECMD_SetEnc;
+
+    return out;
+  }
+
+  Comm::ckw_map_t Comm::loadKeywordMap() {
+    Comm::ckw_map_t out;
+
+    out["deg"] = EKW_Degree;
+    out["abs"] = EKW_Absolute;
+    out["rel"] = EKW_Relative;
+
+    out["dm"] = EKW_Diameter;
+    out["rp"] = EKW_Report;
+
+    return out;
+  }
+
+  const Comm::cmd_map_t Comm::cmdIdMap = Comm::loadCommandMap();
+  const Comm::ckw_map_t Comm::cmdKwMap = Comm::loadKeywordMap();
+
+  Comm::cmd_id_t Comm::getCommandId(const char* in) {
+    cmd_map_t::const_iterator it;
+    for(it = Comm::cmdIdMap.begin();it != Comm::cmdIdMap.end(); ++it) {
+      if(it->first == in) {
+        gLogger.printf("DBG:%s,%s,%i\n", in, it->first.c_str(), (it->first == in));
+        return it->second;
+      }
+    }
+    return ECMD_None;
+  }
+
+  Comm::cmd_kw_t Comm::getKeywordId(const char* in) {
+    ckw_map_t::const_iterator it;
+    for(it = Comm::cmdKwMap.begin();it != Comm::cmdKwMap.end(); ++it)
+      if(it->first == in) return it->second;
+    return EKW_None;
+  }
+
+  void msg_ack(const Message& msg) {
     std::stringstream buf;
     buf << "<1:ACK," << msg.id.c_str() << ',' << msg.argc << '>';
     gLogger.printf("%s\n", buf.str().c_str());
   }
 
-  void msg_debug(const rtx::Protocol::Message& msg) {
+  void msg_nocmd(const Message& msg) {
+    std::stringstream buf;
+
+    buf << "<1:UNK";
+    for(size_t i = 0; i < msg.argc; i++)
+      buf << ',' << msg.argv[i];
+    buf << '>';
+
+    gLogger.printf("%s\n", buf.str().c_str());
+  }
+
+  void msg_debug(const Message& msg) {
     std::stringstream buf;
 
     buf << "<1:ARG";
@@ -30,9 +89,104 @@ namespace usr {
     gLogger.printf("%s\n", buf.str().c_str());
   }
 
-  void Comm::onMessage(const rtx::Protocol::Message& msg) {
-    msg_ack(msg);
-    msg_debug(msg);
+
+  Comm::cmd_res_t Comm::callback(cmd_id_t cId, const Message& msg) {
+    switch(cId)
+    {
+      case ECMD_SetSpeed:
+        return onSetSpeed(msg);
+      case ECMD_Move:
+        return onMove(msg);
+      case ECMD_Turn:
+        return onTurn(msg);
+      case ECMD_Omni:
+        return onOmni(msg);
+      case ECMD_SetPid:
+        return onSetPid(msg);
+      case ECMD_SetEnc:
+        return onSetEnc(msg);
+      default:
+        return CRES_NoCmd;
+    }
+  }
+
+
+  void Comm::onMessage(const Message& msg) {
+    cmd_id_t cId = getCommandId(msg.argv[0]);
+    cmd_res_t res = callback(cId, msg);
+
+    switch(res) {
+      case CRES_NoCmd:
+        msg_nocmd(msg);
+        break;
+      case CRES_OK:
+        msg_ack(msg);
+        msg_debug(msg);
+        break;
+    }
+  }
+
+  Comm::cmd_res_t Comm::onSetSpeed(const Message& msg) {
+    //TODO: Motor control
+
+    std::stringstream buf;
+    buf << "<1:TODO,onSetSpeed," << msg.id.c_str() << ',' << msg.argc << '>';
+    gLogger.printf("%s\n", buf.str().c_str());
+
+    return CRES_OK;
+  }
+
+  Comm::cmd_res_t Comm::onMove(const Message& msg) {
+    //TODO: omni Move
+
+    std::stringstream buf;
+    buf << "<1:TODO,onMove," << msg.id.c_str() << ',' << msg.argc << '>';
+    gLogger.printf("%s\n", buf.str().c_str());
+
+    return CRES_OK;
+  }
+
+  Comm::cmd_res_t Comm::onTurn(const Message& msg) {
+    //TODO: omni Turn
+
+    std::stringstream buf;
+    buf << "<1:TODO,onTurn," << msg.id.c_str() << ',' << msg.argc << '>';
+    gLogger.printf("%s\n", buf.str().c_str());
+
+    return CRES_OK;
+  }
+
+  Comm::cmd_res_t Comm::onOmni(const Message& msg) {
+    //TODO: Omni drive
+
+    std::stringstream buf;
+    buf << "<1:TODO,onOmni," << msg.id.c_str() << ',' << msg.argc << '>';
+    gLogger.printf("%s\n", buf.str().c_str());
+
+    return CRES_OK;
+  }
+
+  Comm::cmd_res_t Comm::onSetPid(const Message& msg) {
+    //TODO: Pid configuration
+    std::stringstream buf;
+    buf << "<1:TODO,onSetPid," << msg.id.c_str() << ',' << msg.argc << '>';
+    gLogger.printf("%s\n", buf.str().c_str());
+
+    return CRES_OK;
+  }
+
+  Comm::cmd_res_t Comm::onSetEnc(const Message& msg) {
+    std::stringstream buf;
+    buf << "<1:TODO,onSetEnc," << msg.id.c_str() << ',' << msg.argc << '>';
+    gLogger.printf("%s\n", buf.str().c_str());
+
+    if(msg.argv[0] == "dm") {
+      //TODO: Set diameter
+    } else if(msg.argv[0] == "rp") {
+      //TODO: Encoder reporting
+    }
+
+    return CRES_OK;
   }
 
 }
--- lib/utmb16/usr_Comm.hpp
+++ lib/utmb16/usr_Comm.hpp
@@ -9,14 +9,86 @@
 #ifndef USR_COMM_H
 #define USR_COMM_H
 
+#include <string>
+#include <map>
+
 #include "rtx_Comm.hpp"
 
+using namespace rtx;
+
 namespace usr {
+
+  //sp,<motId>,<{deg/abs/rel(default)}>,<value>
+  //mv,<{deg/abs/rel(default)}>,<value>,<dir(degrees)>
+  //tr,
+
+  /*
+  pd P, I, D
+  en dm(set diameter), rp(report)
+  */
+
+  typedef Protocol::Message Message;
 
   class Comm : public rtx::Comm
   {
   public:
-    void onMessage(const rtx::Protocol::Message&);
+
+    // FIXME: Use scoped enum when c11 is available.
+    enum cmd_id_t {
+      ECMD_None,
+
+      ECMD_SetSpeed,
+      ECMD_Move,
+      ECMD_Turn,
+      ECMD_Omni,
+
+      ECMD_SetPid,
+      ECMD_SetEnc,
+    };
+
+    enum cmd_kw_t {
+      EKW_None,
+
+      EKW_Degree,
+      EKW_Absolute,
+      EKW_Relative,
+
+      EKW_Diameter,
+      EKW_Report,
+    };
+
+    enum cmd_res_t {
+      CRES_None   = -10,
+      CRES_ERR    = -3,
+      CRES_NoCmd  = -2,
+      CRES_OK     =  0,
+      CRES_DONE   =  1,
+    };
+
+    typedef std::map<std::string, cmd_id_t> cmd_map_t;
+    typedef std::map<std::string, cmd_kw_t> ckw_map_t;
+
+    cmd_res_t callback(cmd_id_t, const Message&);
+
+    void onMessage(const Message&);
+
+    cmd_res_t onSetSpeed(const Message&);
+    cmd_res_t onMove(const Message&);
+    cmd_res_t onTurn(const Message&);
+    cmd_res_t onOmni(const Message&);
+
+    cmd_res_t onSetPid(const Message&);
+    cmd_res_t onSetEnc(const Message&);
+
+    static cmd_map_t loadCommandMap();
+    static ckw_map_t loadKeywordMap();
+
+    static const cmd_map_t cmdIdMap;
+    static const ckw_map_t cmdKwMap;
+
+    static cmd_id_t getCommandId(const char*);
+    static cmd_kw_t getKeywordId(const char*);
+
   };
 
 }
