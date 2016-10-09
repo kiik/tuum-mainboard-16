0576db34a15eeddd3e01f2b65955f955f262f0be
Meelik Kiik <kiik.meelik@gmail.com>
2016-10-10 02:24:56 +0300

    Implement generic PID controller.

--- lib/rtx/rtx_PID.cpp
+++ lib/rtx/rtx_PID.cpp
@@ -2,7 +2,7 @@
  *  @brief PID controller implementation.
  *
  *  @authors Meelik Kiik
- *  @date 8. October 2016
+ *  @date 9. October 2016
  *  @version 0.1
  */
 
@@ -10,9 +10,42 @@
 
 namespace rtx {
 
-  PID::PID()
+  float gP = 1.4;
+  float gI = 0.02;
+  float gD = 0.02;
+
+  PID::PID():
+    P(gP), I(gI), D(gD),
+    m_p(0.0), m_i(0.0), m_d(0.0),
+    m_lerr(0)
   {
 
   }
+
+  PID::pid_size_t PID::run(pid_size_t err, float dt) {
+    m_p = err;
+
+    m_i += err * dt;
+    if(m_i > 1.0) m_i = 1.0;
+    else if(m_i < -1.0) m_i = -1.0;
+
+
+    m_d = (err - m_lerr) / dt;
+    m_lerr = err;
+
+    return P * m_p + I * m_i + D * m_d;
+  }
+
+  void PID::setP(pid_size_t v) { P = v; }
+  void PID::setI(pid_size_t v) { I = v; }
+  void PID::setD(pid_size_t v) { D = v; }
+
+  void PID::setPID(pid_size_t p, pid_size_t i, pid_size_t d) {
+    setP(p); setI(i), setD(d);
+  }
+
+  PID::pid_size_t PID::_p() { return m_p * gP; }
+  PID::pid_size_t PID::_i() { return m_i * gI; }
+  PID::pid_size_t PID::_d() { return m_d * gD; }
 
 }
--- lib/rtx/rtx_PID.hpp
+++ lib/rtx/rtx_PID.hpp
@@ -2,20 +2,41 @@
  *  @brief PID controller.
  *
  *  @authors Meelik Kiik
- *  @date 8. October 2016
+ *  @date 9. October 2016
  *  @version 0.1
  */
 
 #ifndef RTX_PID_H
 #define RTX_PID_H
 
+#include "mbed.h"
+
 namespace rtx {
+
+  extern float gP, gI, gD;
 
   class PID
   {
   public:
+    typedef float pid_size_t;
+
     PID();
 
+    pid_size_t run(pid_size_t, float);
+
+    void setP(pid_size_t);
+    void setI(pid_size_t);
+    void setD(pid_size_t);
+    void setPID(pid_size_t, pid_size_t, pid_size_t);
+
+    pid_size_t _p();
+    pid_size_t _i();
+    pid_size_t _d();
+
+  private:
+    pid_size_t P, I, D, m_val;
+    pid_size_t m_p, m_i, m_d;
+    pid_size_t m_lerr;
   };
 
 }
