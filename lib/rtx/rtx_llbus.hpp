9cad8aee2b67cbea9be4004ebeffb87187a70415
Meelik Kiik <kiik.meelik@gmail.com>
2017-10-04 15:04:07 +0300

    Use default serial implementation.

--- lib/rtx/rtx_llbus.hpp
+++ lib/rtx/rtx_llbus.hpp
@@ -10,12 +10,12 @@
 #define RTX_LLBUS_H
 
 #include <mbed.h>
-#include <USBSerial.h>
+// #include <USBSerial.h>
 
-#define llbus_t USBSerial
+#define llbus_t Serial
 
-#define llbus_decl() USBSerial gBus
-//#define llbus_decl() Serial gBus(USBTX, USBRX)
+// #define llbus_decl() USBSerial gBus
+#define llbus_decl() Serial gBus(USBTX, USBRX)
 
 namespace rtx { namespace llb {
 
